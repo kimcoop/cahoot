ed133b53ba51405ec2ea37057d18bdaea189d65e
Kim Cooperrider <kim.cooperrider@gmail.com>
2012-10-13 13:45:04 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cahoot::Application.config.secret_token = '9e0e8fe6f985f2ad4fa89903717381de151aed12fd6ad98bf32858d0e43beff32d85dbe0c39201c0d75b58d5293900ad9da872e84480ad9c93e2da83b20d4a31'
